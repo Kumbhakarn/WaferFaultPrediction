bbd2da3f90011f950b11a006e63fb3f8c2aff67f
Kumbhakarn <akashkumbhakarn@gmail.com>
2024-12-02 19:31:21 +0530

    First Commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Sensor Project",
+    version="0.0.1",
+    author="Akash Kumbhakarn",
+    author_email="akashkumbhakarn@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
---
+++ src/constant/__init__.py
@@ -0,0 +1,3 @@
+AWS_S3_BUCKET_NAME = "sensor-deployment"
+MONGO_DATABASE_NAME = "New_Data_Wafers"
+MONGO_COLLECTION_NAME = "sensor"
